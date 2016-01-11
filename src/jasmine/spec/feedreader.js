8bcd9013d90a7629a238aad6719d7f5e59ba15e7
Artur Jonczyk <artur.jonczyk@gmail.com>
2016-01-12 00:59:21 +0100

    Fixed the problem with async test according to suggestion, and improved few other small things according to suggestion

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -27,7 +27,6 @@ $(function() {
         */
         it('has url defined, and is not empty', function () {
             allFeeds.forEach(function (feed) {
-                expect(feed.url).toBeDefined();
                 expect(feed.url).toBeTruthy();
             });
         });
@@ -37,7 +36,6 @@ $(function() {
         */
          it('has name defined, and is not empty', function () {
              allFeeds.forEach(function (feed) {
-                 expect(feed.name).toBeDefined();
                  expect(feed.name).toBeTruthy();
              });
          });
@@ -46,12 +44,9 @@ $(function() {
     /* Checking the menu */
     describe('The menu', function () {
 
-        var $body, $menuButton;
-
-        beforeEach(function () {
-            $body = $('body');
+        var $body = $('body'),
             $menuButton = $('.menu-icon-link');
-        });
+
         /* when we loading the app is the menu hidden by default. */
         it('is hidden by default', function () {
             expect($body.hasClass('menu-hidden')).toBeTruthy();
@@ -60,12 +55,12 @@ $(function() {
         /* Will the menu changes visibility when the menu icon is clicked?
         * Does the menu display when clicked and does it hide when clicked again?
         */
-          it('display/hide when clicked', function () {
-              $menuButton.trigger('click');
-              expect($body.hasClass('menu-hidden')).toBeFalsy();
-              $menuButton.trigger('click');
-              expect($body.hasClass('menu-hidden')).toBeTruthy();
-          });
+        it('display/hide when clicked', function () {
+            $menuButton.trigger('click');
+            expect($body.hasClass('menu-hidden')).toBeFalsy();
+            $menuButton.trigger('click');
+            expect($body.hasClass('menu-hidden')).toBeTruthy();
+        });
     });
 
     /* Checking the "Initial Entries" */
@@ -79,10 +74,9 @@ $(function() {
              loadFeed(0, done);
          });
 
-        it('should load at least a single .entry element', function (done) {
-            var numOfEntries = $('.feed').find('.entry').length;
+        it('should load at least a single .entry element', function () {
+            var numOfEntries = $('.feed .entry').length;
             expect(numOfEntries).toBeGreaterThan(0);
-            done();
         });
 
     });
@@ -93,11 +87,13 @@ $(function() {
          */
         var content1, content2;
 
-        beforeEach(function () {
+        beforeEach(function (done) {
             //  first load
-            loadFeed(1);
-            // saving the content
-            content1 = $('.feed').html();
+            loadFeed(1, function () {
+                // saving the content
+                content1 = $('.feed').html();
+                done();
+            });
         });
 
         it('is loading deferent content when clicked diferent feed', function (done) {
